bed1cc9de18f182c45b2fbfca84528926a18b296
tangl <2898387914@qq.com>
2023-09-04 19:47:10 +0800

    【A_dev】NA 【问题单号】NA 【影响性】NA

---
+++ gtest_gmock/inc/A.hpp
@@ -0,0 +1,20 @@
+//
+// Created by tangl on 2023/9/4.
+//
+
+#ifndef GTEST_GMOCK_A_HPP
+#define GTEST_GMOCK_A_HPP
+#include <iostream>
+
+
+class A
+{
+public:
+    void ShowA()
+    {
+        std::cout << "this is A."<<std::endl;
+    }
+};
+
+
+#endif//GTEST_GMOCK_A_HPP
